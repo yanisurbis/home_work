2f4e5f66f7cf6f707c1f79d80fcab98cf3ac0ce1
Yanis <yanis.urbis@gmail.com>
2020-06-11 21:22:01 +0300

    Try to parse models.go

--- hw09_generator_of_validators/models/models.go
+++ hw09_generator_of_validators/models/models.go
@@ -3,14 +3,14 @@ package models
 type UserRole string
 
 // NOTE: Several struct specs in one type declaration are allowed
-//go:generate /$GOPATH/bin/go-validate
+//go:generate go-validate
 type User struct {
-	ID     string `json:"id" validate:"len:36"`
-	Name   string
+	//ID     string `json:"id" validate:"len:36"`
+	//Name   string
 	Age    int      `validate:"min:18|max:50"`
-	Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-	Role   UserRole `validate:"in:admin,stuff"`
-	Phones []string `validate:"len:11"`
+	//Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+	//Role   UserRole `validate:"in:admin,stuff"`
+	//Phones []string `validate:"len:11"`
 }
 
 type ValidationError struct {
