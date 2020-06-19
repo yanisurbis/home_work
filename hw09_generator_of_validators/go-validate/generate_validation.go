c1910f6d67e2b61bbba13385b86a83a2c975456a
Yanis <yanis.urbis@gmail.com>
2020-06-19 11:37:30 +0300

    Add basic generate validation

---
+++ hw09_generator_of_validators/go-validate/generate_validation.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"strings"
+)
+
+//import "strings"
+
+//func appendErrorStr(fieldName string, )
+
+func generateFieldValidation(fieldName string,validation FieldValidation) string {
+	//validation = FieldValidation{
+	//	Type:  "min",
+	//	Value: "18",
+	//}
+	//fieldName := "Age"
+	//fieldType := "int"
+	//fieldTag := "min:18|max:50"
+
+	validationString := ""
+
+	if validation.Type == "min" {
+		value := validation.Value.(string)
+			validationString += `
+if x.` + fieldName + ` < ` + value + ` {
+	errs = append(errs, ValidationError{Field: "` + fieldName + `", Err: "Should be more than ` + value + `"})
+}
+`
+	} else if validation.Type == "max" {
+		value := validation.Value.(string)
+			validationString += `
+if x.` + fieldName + ` > ` + value + ` {
+	errs = append(errs, ValidationError{Field: "` + fieldName + `", Err: "Should be less than ` + value + `"})
+}
+`
+	} else if validation.Type == "len" {
+		value := validation.Value.(string)
+		validationString += `
+if len(x.` + fieldName + `) > ` + value + ` {
+	errs = append(errs, ValidationError{Field: "` + fieldName + `", Err: "Should be less than ` + value + `"})
+}
+`
+	} else if validation.Type == "regexp" {
+		value := validation.Value.(string)
+		validationString += `
+{
+	match, _ := regexp.MatchString("` + value + `", x.` + fieldName +`)
+	if !match {
+		errs = append(errs, ValidationError{Field: "` + fieldName + `", Err: "Should satisfy the pattern ` + value + `"})
+	}
+}
+`
+	} else if validation.Type == "in" {
+		values := validation.Value.([]string)
+		validationString += `
+{
+	isIn := false
+	for _, v := range []int{`+ strings.Join(values, ",") +`} {
+		if v == x.`+ fieldName + ` {
+			isIn = true
+		}
+	}
+	if !isIn {
+		errs = append(errs, ValidationError{Field: "` + fieldName + `", Err: "Element should be one of ` + strings.Join(values, ",") + `"})
+	}
+}
+`
+	}
+
+	//fieldRules := strings.Split(fieldTag, "|")
+	//for _, fieldRule := range fieldRules {
+	//	ruleNameAndValue := strings.Split(fieldRule, ":")
+	//	ruleName := ruleNameAndValue[0]
+	//	ruleValue := ruleNameAndValue[1]
+	//
+	//
+	//}
+
+	return validationString
+}
