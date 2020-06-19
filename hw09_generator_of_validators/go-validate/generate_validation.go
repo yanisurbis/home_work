524bd9de76e7f934600ecd13126db7bb33077f48
yan_zaycev <yzaytsev@voximplant.com>
2020-06-19 21:53:29 +0300

    Generate structures validation

--- hw09_generator_of_validators/go-validate/generate_validation.go
+++ hw09_generator_of_validators/go-validate/generate_validation.go
@@ -8,7 +8,51 @@ import (
 
 //func appendErrorStr(fieldName string, )
 
-func generateFieldValidation(fieldName string,validation FieldValidation) string {
+//type FieldDescription struct {
+//	Name string
+//	Type string
+//	Validations []FieldValidation
+//}
+//
+//type InterfaceDescription struct {
+//	Name string
+//	Fields []FieldDescription
+//}
+
+func generateMultipleStructValidations(structures []InterfaceDescription) string {
+	validations := ""
+
+	for _, structure := range structures {
+		validations += generateStructValidation(structure)
+	}
+
+	return validations
+}
+
+func generateStructValidation(structure InterfaceDescription) string {
+	validations := func(fields []FieldDescription) string {
+		validationContent := ""
+
+		for _, field := range fields {
+			for _, fieldValidation := range field.Validations {
+				validationContent += generateFieldValidation(field.Name, fieldValidation)
+			}
+		}
+
+		return validationContent
+	}(structure.Fields)
+
+	return `
+func (x ` + structure.Name + `) Validate()  ([]ValidationError, error) {
+errs := []ValidationError{}
+	` + validations + `
+
+return errs, nil
+}
+`
+}
+
+func generateFieldValidation(fieldName string, validation FieldValidation) string {
 	//validation = FieldValidation{
 	//	Type:  "min",
 	//	Value: "18",
@@ -21,14 +65,14 @@ func generateFieldValidation(fieldName string,validation FieldValidation) string
 
 	if validation.Type == "min" {
 		value := validation.Value.(string)
-			validationString += `
+		validationString += `
 if x.` + fieldName + ` < ` + value + ` {
 	errs = append(errs, ValidationError{Field: "` + fieldName + `", Err: "Should be more than ` + value + `"})
 }
 `
 	} else if validation.Type == "max" {
 		value := validation.Value.(string)
-			validationString += `
+		validationString += `
 if x.` + fieldName + ` > ` + value + ` {
 	errs = append(errs, ValidationError{Field: "` + fieldName + `", Err: "Should be less than ` + value + `"})
 }
@@ -44,7 +88,7 @@ if len(x.` + fieldName + `) > ` + value + ` {
 		value := validation.Value.(string)
 		validationString += `
 {
-	match, _ := regexp.MatchString("` + value + `", x.` + fieldName +`)
+	match, _ := regexp.MatchString("` + value + `", x.` + fieldName + `)
 	if !match {
 		errs = append(errs, ValidationError{Field: "` + fieldName + `", Err: "Should satisfy the pattern ` + value + `"})
 	}
@@ -55,8 +99,8 @@ if len(x.` + fieldName + `) > ` + value + ` {
 		validationString += `
 {
 	isIn := false
-	for _, v := range []int{`+ strings.Join(values, ",") +`} {
-		if v == x.`+ fieldName + ` {
+	for _, v := range []int{` + strings.Join(values, ",") + `} {
+		if v == x.` + fieldName + ` {
 			isIn = true
 		}
 	}
