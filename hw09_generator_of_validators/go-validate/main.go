338f0f3cf24b131e7d22338f53538a46db55fc96
Yanis <yanis.urbis@gmail.com>
2020-06-29 22:28:08 +0300

    refactor

--- hw09_generator_of_validators/go-validate/main.go
+++ hw09_generator_of_validators/go-validate/main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"log"
 	"os"
 )
 
 func writeToFile(str string, path string) {
 	f, err := os.Create(path)
+	defer f.Close()
 	if err != nil {
-		log.Println(err)
+		panic(err)
 	}
 	f.WriteString(str)
-	f.Close()
 }
 
 func main() {
 	filename := os.Getenv("GOFILE")
-	// "models/models.go"
 	res := generateValidation(extractInterfaceDescriptions(filename))
-	writeToFile(res, "models/models_validation_generated.go")
+	writeToFile(res, os.Getenv("PWD") + "/models/models_validation_generated.go")
 }
