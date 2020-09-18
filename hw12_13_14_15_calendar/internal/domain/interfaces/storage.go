06981d5e89d3dd3a2b745077042d861d06fe3aac
Yanis <yanis.urbis@gmail.com>
2020-09-18 21:56:55 +0300

    event service, v1

---
+++ hw12_13_14_15_calendar/internal/domain/interfaces/storage.go
@@ -0,0 +1,19 @@
+package domain
+
+import (
+	"calendar/internal/domain/entities"
+	"context"
+	"time"
+)
+
+type EventStorage interface {
+	Connect(ctx context.Context, dsn string) error
+	Close() error
+	AddEvent(event entities.Event) error
+	UpdateEvent(userID entities.ID, event entities.Event) error
+	DeleteEvent(userID entities.ID, eventID entities.ID) error
+	GetEventsDay(userID entities.ID, from time.Time) ([]entities.Event, error)
+	GetEventsWeek(userID entities.ID, from time.Time) ([]entities.Event, error)
+	GetEventsMonth(userID entities.ID, from time.Time) ([]entities.Event, error)
+	GetEvent(userID entities.ID, id entities.ID) (entities.Event, error)
+}
---
+++ hw12_13_14_15_calendar/internal/domain/services/event.go
@@ -0,0 +1,80 @@
+package domain
+
+import (
+	"calendar/internal/domain/entities"
+	"calendar/internal/domain/interfaces"
+	"context"
+	"time"
+)
+
+type EventService struct {
+	EventStorage domain.EventStorage
+}
+
+func (es *EventService) AddEvent(ctx context.Context, title string, startAt time.Time, endAt time.Time,
+									description string, notifyAt time.Time, userID entities.ID) (*entities.Event, error) {
+	// TODO: add validation
+	event := entities.Event{
+		Title:       title,
+		StartAt:     startAt,
+		EndAt:       endAt,
+		Description: description,
+		NotifyAt:    notifyAt,
+		UserID:      userID,
+	}
+
+	err := es.EventStorage.AddEvent(event)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+}
+
+func (es *EventService) UpdateEvent(ctx context.Context, eventID entities.ID, title string, startAt time.Time, endAt time.Time,
+	description string, notifyAt time.Time, userID entities.ID) (*entities.Event, error) {
+	event := entities.Event{
+		ID: 	     eventID,
+		Title:       title,
+		StartAt:     startAt,
+		EndAt:       endAt,
+		Description: description,
+		NotifyAt:    notifyAt,
+		UserID:      userID,
+	}
+
+	event, err := es.EventStorage.GetEvent(userID, eventID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = es.EventStorage.UpdateEvent(userID, event)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+}
+
+func (es *EventService) DeleteEvent(ctx context.Context, userID entities.ID, eventID entities.ID) (*entities.Event, error) {
+	event, err := es.EventStorage.GetEvent(userID, eventID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = es.EventStorage.DeleteEvent(userID, eventID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+}
+
+func (es *EventService) GetEvents(ctx context.Context, userID entities.ID, period string, from time.Time) ([]entities.Event, error) {
+	return es.EventStorage.GetEventsWeek(userID, from)
+}
