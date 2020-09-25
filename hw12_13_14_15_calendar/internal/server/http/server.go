c023d4f724735dc5c14e6e47a87637c7c2c2aa59
Yanis <yanis.urbis@gmail.com>
2020-09-25 21:45:56 +0300

    add more todos

--- hw12_13_14_15_calendar/internal/server/http/server.go
+++ hw12_13_14_15_calendar/internal/server/http/server.go
@@ -20,6 +20,99 @@ type Instance struct {
 const repositoryKey = "repository"
 const userIdKey = "userId"
 
+func (s *Instance) Start(eventService domain.EventService) error {
+
+	router := gin.Default()
+	router.Use(UserIDMiddleware())
+
+	router.DELETE("/event/:id", func(c *gin.Context) {
+		// TODO: err -> isErrHandled
+		deleteEventRequest, err := prepareDeleteEventRequest(c)
+
+		if err != nil {
+			return
+		}
+
+		deletedEvent, err := eventService.DeleteEvent(c, deleteEventRequest)
+
+		if err != nil {
+			if err == domain3.ErrForbidden {
+				c.String(http.StatusForbidden, "don't have access")
+				return
+			} else if err == domain3.ErrNotFound {
+				c.String(http.StatusNotFound, "event is not found")
+				return
+			} else {
+				c.String(http.StatusInternalServerError, "error")
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, deletedEvent)
+	})
+
+	router.GET("/events", func(c *gin.Context) {
+		getEventsRequest, err := prepareGetEventsRequest(c)
+
+		if err != nil {
+			return
+		}
+
+		events, err := eventService.GetEvents(c, getEventsRequest)
+
+		if err != nil {
+			c.String(http.StatusInternalServerError, "error")
+			return
+		}
+
+		c.JSON(http.StatusOK, events)
+	})
+
+	router.POST("/event", func(c *gin.Context) {
+		addEventRequest, err := prepareAddEventRequest(c)
+
+		if err != nil {
+			return
+		}
+
+		addedEvent, err := eventService.AddEvent(c, addEventRequest)
+
+		if err != nil {
+			c.String(http.StatusInternalServerError, "error")
+			return
+		}
+
+		c.JSON(http.StatusOK, addedEvent)
+	})
+
+	router.PUT("/event/:id", func(c *gin.Context) {
+		updateEventRequest, err := prepareUpdateEventRequest(c)
+
+		if err != nil {
+			return
+		}
+
+		addedEvent, err := eventService.UpdateEvent(c, updateEventRequest)
+
+		if err != nil {
+			c.String(http.StatusInternalServerError, "error")
+			return
+		}
+
+		c.JSON(http.StatusOK, addedEvent)
+	})
+
+	fmt.Println("server starting at port :8080")
+
+	return router.Run(":8080")
+}
+
+func (s *Instance) Stop(ctx context.Context) error {
+	// TODO: check how to remove domain1, domain2, domain3
+	// TODO: put correct shutdown
+	return s.instance.Shutdown(ctx)
+}
+
 func getTimeFromTimestamp(timestamp string) (time.Time, error) {
 	fromInt, err := strconv.Atoi(timestamp)
 	if err != nil {
@@ -27,7 +120,6 @@ func getTimeFromTimestamp(timestamp string) (time.Time, error) {
 	}
 
 	from := time.Unix(int64(fromInt), 0)
-	fmt.Println("date -> " + from.String())
 
 	return from, nil
 }
@@ -158,94 +250,4 @@ func prepareUpdateEventRequest(c *gin.Context) (*entities.UpdateEventRequest, er
 	}
 
 	return &eventUpdate, nil
-}
-
-func (s *Instance) Start(eventService domain.EventService) error {
-
-	router := gin.Default()
-	router.Use(UserIDMiddleware())
-
-	router.DELETE("/event/:id", func(c *gin.Context) {
-		deleteEventRequest, err := prepareDeleteEventRequest(c)
-
-		if err != nil {
-			return
-		}
-
-		deletedEvent, err := eventService.DeleteEvent(c, deleteEventRequest)
-
-		if err != nil {
-			if err == domain3.ErrForbidden {
-				c.String(http.StatusForbidden, "don't have access")
-				return
-			} else if err == domain3.ErrNotFound {
-				c.String(http.StatusNotFound, "event is not found")
-				return
-			} else {
-				c.String(http.StatusInternalServerError, "error")
-				return
-			}
-		}
-
-		c.JSON(http.StatusOK, deletedEvent)
-	})
-
-	router.GET("/events", func(c *gin.Context) {
-		getEventsRequest, err := prepareGetEventsRequest(c)
-
-		if err != nil {
-			return
-		}
-
-		events, err := eventService.GetEvents(c, getEventsRequest)
-
-		if err != nil {
-			c.String(http.StatusInternalServerError, "error")
-			return
-		}
-
-		c.JSON(http.StatusOK, events)
-	})
-
-	router.POST("/event", func(c *gin.Context) {
-		addEventRequest, err := prepareAddEventRequest(c)
-
-		if err != nil {
-			return
-		}
-
-		addedEvent, err := eventService.AddEvent(c, addEventRequest)
-
-		if err != nil {
-			c.String(http.StatusInternalServerError, "error")
-			return
-		}
-
-		c.JSON(http.StatusOK, addedEvent)
-	})
-
-	router.PUT("/event/:id", func(c *gin.Context) {
-		updateEventRequest, err := prepareUpdateEventRequest(c)
-
-		if err != nil {
-			return
-		}
-
-		addedEvent, err := eventService.UpdateEvent(c, updateEventRequest)
-
-		if err != nil {
-			c.String(http.StatusInternalServerError, "error")
-			return
-		}
-
-		c.JSON(http.StatusOK, addedEvent)
-	})
-
-	fmt.Println("server starting at port :8080")
-
-	return router.Run(":8080")
-}
-
-func (s *Instance) Stop(ctx context.Context) error {
-	return s.instance.Shutdown(ctx)
 }
