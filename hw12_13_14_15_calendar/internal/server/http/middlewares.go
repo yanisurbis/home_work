6d6e530be17d8ab1d43b6bb7f9fe8e0463a391e5
Yanis <yanis.urbis@gmail.com>
2020-10-05 20:38:31 +0300

    delete unused middlewares

--- hw12_13_14_15_calendar/internal/server/http/middlewares.go
+++ hw12_13_14_15_calendar/internal/server/http/middlewares.go
@@ -3,35 +3,9 @@ package http_server
 import (
 	"calendar/internal/domain/entities"
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
 	"strconv"
-	"time"
 )
-
-
-func panicMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-				log.Println("recovered: ", err)
-				http.Error(w, "Internal server error", 500)
-			}
-		}()
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func logMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func(t time.Time) {
-			log.Println(r.RemoteAddr+" "+r.Method+" "+r.Host+" "+r.UserAgent(), " ", time.Since(t).Milliseconds(), "ms")
-		}(time.Now())
-
-		next.ServeHTTP(w, r)
-	})
-}
 
 func UserIDMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
