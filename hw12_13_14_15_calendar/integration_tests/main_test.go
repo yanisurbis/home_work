f553b3a525851ff7170a98b6ee66715b056ed32e
Yanis Urbis <yanis.urbis@gmail.com>
2020-11-03 10:18:40 +0300

    Add base for crud test

--- hw12_13_14_15_calendar/integration_tests/main_test.go
+++ hw12_13_14_15_calendar/integration_tests/main_test.go
@@ -1,13 +1,61 @@
 package memory
 
 import (
+	grpcclient "calendar/internal/client/grpc"
+	"calendar/internal/domain/entities"
+	"context"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
+
+func TestCRUD(t *testing.T) {
+	client := grpcclient.NewClient()
+	err := client.Start(context.Background())
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	addEventRequest := entities.AddEventRequest{
+		Title:       "Event from test",
+		StartAt:     time.Now().Add(3 * time.Hour),
+		EndAt:       time.Now().Add(5 * time.Hour),
+		Description: "Description from test",
+		NotifyAt:    time.Now().Add(4 * time.Hour),
+		UserID:      1,
+	}
+
+	err = client.AddEvent(addEventRequest)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	events, err := client.GetEventsDay(entities.GetEventsRequest{
+		UserID: 1,
+		Type:   "day",
+		From:   time.Now(),
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	addedEvent := new(entities.Event)
+	for _, event := range events {
+		if event.Title == addEventRequest.Title {
+			addedEvent = &event
+		}
+	}
+
+	assert.Equal(t, addedEvent.Description, addEventRequest.Description)
+	assert.Equal(t, addedEvent.Title, addEventRequest.Title)
+	//assert.Equal(t, addedEvent.StartAt, addEventRequest.StartAt)
+	//assert.Equal(t, addedEvent.EndAt, addEventRequest.EndAt)
+	assert.Equal(t, addedEvent.Description, addEventRequest.Description)
+	assert.Equal(t, addedEvent.UserID, addEventRequest.UserID)
+}
 
 func TestIntegration(t *testing.T) {
 	t.Run("random stuff", func(t *testing.T) {
-		assert.Equal(t, 123, 123, "they should be equal")
+		TestCRUD(t)
 	})
 }
-
