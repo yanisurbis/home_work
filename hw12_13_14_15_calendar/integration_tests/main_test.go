978713859076c02653c4296523a7e06e1398f414
Yanis Urbis <yanis.urbis@gmail.com>
2020-11-06 20:50:15 +0300

    Add lists tests

--- hw12_13_14_15_calendar/integration_tests/main_test.go
+++ hw12_13_14_15_calendar/integration_tests/main_test.go
@@ -6,20 +6,46 @@ import (
 	"context"
 	"github.com/stretchr/testify/assert"
 	"log"
+	"strconv"
 	"testing"
 	"time"
 )
 
-func getEvents(client *grpcclient.Client) []entities.Event {
-	events, err := client.GetEventsDay(entities.GetEventsRequest{
+func getEvents(client *grpcclient.Client, request entities.GetEventsRequest) []entities.Event {
+	switch request.Type {
+	case "day":
+		events, err := client.GetEventsDay(request)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return events
+	case "week":
+		events, err := client.GetEventsWeek(request)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return events
+	case "month":
+		events, err := client.GetEventsMonth(request)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return events
+	default:
+		events, err := client.GetEventsDay(request)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return events
+	}
+}
+
+func getEventsDay(client *grpcclient.Client) []entities.Event {
+	return getEvents(client, entities.GetEventsRequest{
 		UserID: 1,
 		Type:   "day",
 		From:   time.Now(),
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	return events
 }
 
 func testCreate(t *testing.T, client *grpcclient.Client) *entities.Event {
@@ -40,7 +66,7 @@ func testCreate(t *testing.T, client *grpcclient.Client) *entities.Event {
 		log.Fatal(err)
 	}
 
-	events := getEvents(client)
+	events := getEventsDay(client)
 
 	addedEvent := new(entities.Event)
 	for _, event := range events {
@@ -58,9 +84,9 @@ func testCreate(t *testing.T, client *grpcclient.Client) *entities.Event {
 		Description: addEventRequest.Description,
 		UserID:      addEventRequest.UserID,
 	})
-	assert.Equal(t, 0, addedEvent.StartAt.Second() - addEventRequest.StartAt.Second())
-	assert.Equal(t, 0, addedEvent.EndAt.Second() - addEventRequest.EndAt.Second())
-	assert.Equal(t, 0, addedEvent.NotifyAt.Second() - addEventRequest.NotifyAt.Second())
+	assert.Equal(t, 0, addedEvent.StartAt.Second()-addEventRequest.StartAt.Second())
+	assert.Equal(t, 0, addedEvent.EndAt.Second()-addEventRequest.EndAt.Second())
+	assert.Equal(t, 0, addedEvent.NotifyAt.Second()-addEventRequest.NotifyAt.Second())
 
 	return addedEvent
 }
@@ -78,7 +104,7 @@ func testUpdate(t *testing.T, client *grpcclient.Client, addedEvent *entities.Ev
 		log.Fatal(err)
 	}
 
-	events := getEvents(client)
+	events := getEventsDay(client)
 
 	updatedEvent := new(entities.Event)
 	for _, event := range events {
@@ -88,27 +114,27 @@ func testUpdate(t *testing.T, client *grpcclient.Client, addedEvent *entities.Ev
 	}
 
 	assert.Equal(t, entities.Event{
-		ID: updatedEvent.ID,
+		ID:          updatedEvent.ID,
 		Title:       updatedEvent.Title,
 		Description: updatedEvent.Description,
 		UserID:      updatedEvent.UserID,
 	}, entities.Event{
-		ID: updateEventRequest.ID,
+		ID:          updateEventRequest.ID,
 		Title:       updateEventRequest.Title,
 		Description: updateEventRequest.Description,
 		UserID:      updateEventRequest.UserID,
 	})
-	assert.Equal(t, 0, updatedEvent.StartAt.Second() - addedEvent.StartAt.Second())
-	assert.Equal(t, 0, updatedEvent.EndAt.Second() - addedEvent.EndAt.Second())
-	assert.Equal(t, 0, updatedEvent.NotifyAt.Second() - addedEvent.NotifyAt.Second())
+	assert.Equal(t, 0, updatedEvent.StartAt.Second()-addedEvent.StartAt.Second())
+	assert.Equal(t, 0, updatedEvent.EndAt.Second()-addedEvent.EndAt.Second())
+	assert.Equal(t, 0, updatedEvent.NotifyAt.Second()-addedEvent.NotifyAt.Second())
 
 	return updatedEvent
 }
 
 func testDelete(t *testing.T, client *grpcclient.Client, addedEvent *entities.Event) {
 	deleteEventRequest := entities.DeleteEventRequest{
-		ID:          addedEvent.ID,
-		UserID:      addedEvent.UserID,
+		ID:     addedEvent.ID,
+		UserID: addedEvent.UserID,
 	}
 
 	err := client.DeleteEvent(deleteEventRequest)
@@ -116,7 +142,7 @@ func testDelete(t *testing.T, client *grpcclient.Client, addedEvent *entities.Ev
 		log.Fatal(err)
 	}
 
-	events := getEvents(client)
+	events := getEventsDay(client)
 
 	found := false
 	for _, event := range events {
@@ -179,6 +205,90 @@ func testCRUDErrors(t *testing.T, client *grpcclient.Client) {
 	}
 }
 
+func checkEventsByIndex(t *testing.T, events []entities.Event, indexes []string) {
+	var titles []string
+	for _, event := range events {
+		titles = append(titles, event.Title)
+	}
+
+	assert.Equal(t, indexes, titles)
+}
+
+func clearEvents(client *grpcclient.Client, events []entities.Event) {
+	for _, event := range events {
+		request := entities.DeleteEventRequest{
+			ID:     event.ID,
+			UserID: event.UserID,
+		}
+		err := client.DeleteEvent(request)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func testLists(t *testing.T, client *grpcclient.Client) {
+	baseTime := time.Now().AddDate(1, 0, 0)
+	baseUserId := 1
+
+	baseEvent := entities.AddEventRequest{
+		Title:       "Base event, title",
+		StartAt:     baseTime,
+		EndAt:       baseTime.AddDate(2, 0, 0),
+		Description: "Base event, description",
+		NotifyAt:    baseTime.Add(-2 * time.Hour),
+		UserID:      baseUserId,
+	}
+
+	dates := []time.Time{
+		baseTime.Add(1 * time.Hour),
+		baseTime.Add(2 * time.Hour),
+		baseTime.Add(3 * time.Hour),
+		baseTime.AddDate(0, 0, 1),
+		baseTime.AddDate(0, 0, 2),
+		baseTime.AddDate(0, 0, 3),
+		baseTime.AddDate(0, 0, 8),
+		baseTime.AddDate(0, 0, 9),
+		baseTime.AddDate(0, 0, 20),
+		baseTime.AddDate(0, 2, 0),
+		baseTime.AddDate(0, 3, 0),
+		baseTime.AddDate(0, 4, 0),
+	}
+
+	for index, date := range dates {
+		indexStr := strconv.Itoa(index)
+		baseEvent.Title = indexStr
+		baseEvent.StartAt = date
+		err := client.AddEvent(baseEvent)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	dayEvents := getEvents(client, entities.GetEventsRequest{
+		UserID: baseUserId,
+		Type:   "day",
+		From:   baseTime,
+	})
+	checkEventsByIndex(t, dayEvents, []string{"0", "1", "2"})
+
+	weekEvents := getEvents(client, entities.GetEventsRequest{
+		UserID: baseUserId,
+		Type:   "week",
+		From:   baseTime,
+	})
+	checkEventsByIndex(t, weekEvents, []string{"0", "1", "2", "3", "4", "5"})
+
+	monthEvents := getEvents(client, entities.GetEventsRequest{
+		UserID: baseUserId,
+		Type:   "month",
+		From:   baseTime,
+	})
+	checkEventsByIndex(t, monthEvents, []string{"0", "1", "2", "3", "4", "5", "6", "7", "8"})
+
+	clearEvents(client, monthEvents)
+}
+
 func TestIntegration(t *testing.T) {
 	client := grpcclient.NewClient()
 	err := client.Start(context.Background())
@@ -191,5 +301,8 @@ func TestIntegration(t *testing.T) {
 	})
 	t.Run("CRUD, basic validations are present", func(t *testing.T) {
 		testCRUDErrors(t, client)
+	})
+	t.Run("Check lists", func(t *testing.T) {
+		testLists(t, client)
 	})
 }
