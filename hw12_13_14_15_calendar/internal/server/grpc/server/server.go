e43488a8c1447d0d98357f40d9f53f402d1fbd28
Yanis Urbis <yanis.urbis@gmail.com>
2020-11-16 20:40:32 +0300

    Add docker compose configuration for tests

--- hw12_13_14_15_calendar/internal/server/grpc/server/server.go
+++ hw12_13_14_15_calendar/internal/server/grpc/server/server.go
@@ -276,6 +276,7 @@ func (s *Server) DeleteEvent(
 }
 
 func (s *Server) Start(eventService domain.EventService, address string) error {
+	log.Println("Address: ", address)
 	lsn, err := net.Listen("tcp", address)
 	if err != nil {
 		return err
