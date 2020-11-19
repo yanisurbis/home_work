c58c180fe86d8ca7a6c67c0fc1d7bb51071c72ae
Yanis Urbis <yanis.urbis@gmail.com>
2020-11-19 19:09:20 +0300

    delete dead code

--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -12,18 +12,8 @@ type Logger interface {
 type Instance struct {
 }
 
-func (i *Instance) Init(path string) error {
+func (i *Instance) Init(_ string) error {
 	log.SetOutput(os.Stdout)
-
-	//If the file doesn't exist, create it or append to the file
-	//file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	//if err != nil {
-	//	log.Fatal(err)
-	//
-	//	return err
-	//}
-	//log.SetOutput(file)
-	//fmt.Println("logger initialized, log file: ", path)
 
 	return nil
 }
